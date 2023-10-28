1ba892592cb88b254cf2399d9523d22bd64a8041
mahat sumit <mahatsumit5@gmail.com>
2023-10-28 11:56:18 +1100

    chat bot fixed

--- server.js
+++ server.js
@@ -19,32 +19,17 @@ app.get("/", (req, res) => {
 
 app.post("/completions", async (req, res) => {
   const { prompt } = req.body;
-  console.log("first");
-  console.log(req.body);
   try {
     const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
     const completion = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
       messages: [
-        {
-          role: "system",
-          content:
-            "You will be provided with a block of text, and your task is to extract a list of keywords from it.",
-        },
-        {
-          role: "user",
-          content:
-            "Black-on-black ware is a 20th- and 21st-century pottery tradition developed by the Puebloan Native American ceramic artists in Northern New Mexico. Traditional reduction-fired blackware has been made for centuries by pueblo artists. Black-on-black ware of the past century is produced with a smooth surface, with the designs applied through selective burnishing or the application of refractory slip. Another style involves carving or incising designs and selectively polishing the raised areas. For generations several families from Kha'po Owingeh and P'ohwhóge Owingeh pueblos have been making black-on-black ware with the techniques passed down from matriarch potters. Artists from other pueblos have also produced black-on-black ware. Several contemporary artists have created works honoring the pottery of their ancestors.",
-        },
-        {
-          role: "assistant",
-          content:
-            "Black-on-black ware, pottery tradition, Puebloan Native American, ceramic artists, Northern New Mexico, reduction-fired blackware, pueblo artists, smooth surface, designs, selective burnishing, refractory slip, carving, incising designs, polishing, generations, families, Kha'po Owingeh, P'ohwhóge Owingeh, pueblos, techniques, matriarch potters, contemporary artists, ancestors.",
-        },
+        { role: "system", content: "You are a helpful assistant." },
+        { role: "user", content: prompt },
       ],
     });
     const { message } = completion.choices[0];
-    res.send({
+    res.json({
       status: "success",
       message,
     });
